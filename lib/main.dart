cc07da2e46d31941094e3508abf729508fe1f116
nehadevarapalli <59365331+nehadevarapalli@users.noreply.github.com>
2021-03-30 00:05:07 +0530

    Add files via upload
    new article page has been added. the old file named 'articles.dart' in the lib folder has to be deleted.

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
 //import 'dart:js';
 
 import 'package:flutter/material.dart';
-import 'package:sozonew/articles.dart';
+import 'package:sozonew/ArticleList.dart';
 import 'package:sozonew/loginpage.dart';
 import 'home_page.dart';
 import 'user_profile.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-
-      initialRoute: '/',
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/':(context)=>Home(),
-        '/first':(context)=>Homepage(),
-        '/second':(context)=>UserProfile(),
-        '/third':(context)=>ArticlePage(),
-      },
-
-    ),
+      MaterialApp(
+        initialRoute: '/',
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/':(context)=>Home(),
+          '/first':(context)=>Homepage(),
+          '/second':(context)=>UserProfile(),
+          '/third':(context)=>ArticleList(title: "Safety Tips"),
+        },
+      ),
   );
 }
